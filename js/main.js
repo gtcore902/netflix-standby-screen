c8db901f3b1a5328255a250d31c2365e0a33bc8d
gtcore902 <gaetan.tremois@gmail.com>
2024-06-03 23:36:35 +0200

    refactor: remove comments

--- js/main.js
+++ js/main.js
@@ -3,23 +3,6 @@ import genres from '../assets/genres.js';
 
 const films = await fetchDatas();
 console.log(films);
-// const films = [
-//   {
-//     url: 'https://media.gqmagazine.fr/photos/622617a15f69a13708143038/16:9/w_1600,h_899,c_limit/avatar%20cover%202.jpg',
-//     title: 'Avatar',
-//     tags: ['Aventure', 'SF'],
-//   },
-//   {
-//     url: 'https://media.ouest-france.fr/v1/pictures/fea25c37890f0cc1e7fdb5e44b19cb84-4386325.jpg?width=1260&client_id=eds&sign=47e41a421c7e7f71c8d072d6ef716b7cc7d06896cf2f53bf4bbfd262cae94a7e',
-//     title: 'Harry Potter',
-//     tags: ['SF'],
-//   },
-//   {
-//     url: 'https://images.affiches-et-posters.com//albums/3/3901/medium/affiche-film-sinister-paysage--1716.jpg',
-//     title: 'Sinister',
-//     tags: ['SF', 'Horreur'],
-//   },
-// ];
 
 function generateNameById(id) {
   const genre = genres.filter((genre) => genre.id === id);
@@ -27,7 +10,7 @@ function generateNameById(id) {
 }
 
 console.log(
-  genres.map((genre, key) => {
+  genres.map((genre) => {
     genre.id === 37;
   })
 );
@@ -46,7 +29,6 @@ const animation = () => {
   }
   tagsElement.textContent = '';
   imageElement.src = '';
-  //   imageElement.src = films[currentState].url;
   imageElement.src =
     'https://image.tmdb.org/t/p/w1280/' + films[currentState].backdrop_path;
   titleElement.textContent = films[currentState].title;
