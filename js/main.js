ccc33256260328886759708ee03c12ad8ab2998f
gtcore902 <gaetan.tremois@gmail.com>
2024-06-04 09:52:58 +0200

    feat: add tag for best movies

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ let films = await fetchDatas(moviesUrl);
 films = films.results;
 let genres = await fetchDatas(genresUrl);
 genres = genres.genres;
+let descriptionElement = document.querySelector('.description');
 let imageElement = document.querySelector('.image');
 let titleElement = document.querySelector('.title');
 let tagsElement = document.querySelector('.tags');
+const statContainer = document.createElement('p');
 const moviesLength = films.length;
 let currentState = 0;
 
+console.log(films.filter((films) => films.vote_average > 7));
 /**
  * Generate genre from id
  * @param {number} id
@@ -32,7 +35,7 @@ const animation = () => {
 
   tagsElement.textContent = '';
   imageElement.src = '';
-
+  statContainer.textContent = '';
   imageElement.src =
     'https://image.tmdb.org/t/p/w1280/' + films[currentState].backdrop_path;
 
@@ -43,6 +46,11 @@ const animation = () => {
     index <= films[currentState].genre_ids.length - 2 &&
       (tagsElement.textContent += ' - ');
   });
+  if (films[currentState].vote_average > 7) {
+    statContainer.innerHTML = '&#128077; <em>Film dans le top 10</em>';
+    statContainer.classList.add('stats');
+    descriptionElement.appendChild(statContainer);
+  }
   currentState += 1;
   // Loop with this animation
   setTimeout(animation, 15000);
